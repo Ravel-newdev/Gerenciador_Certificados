e9eafc61c8094696edb5a5bc467c7fd34658dd58
Menemakerson <viniciusguita64@gmail.com>
2026-06-05 00:33:22 -0300

    removendo o virtual env que foi commitado por engano

--- main.py
+++ main.py
@@ -5,11 +5,7 @@ from random import randint
 app = FastAPI(root_path="/api/versao1")
 """
 TODO:
--PARTICIPANTE{
--ID
--CPF
--NOME
-}
+-Exclusao participante
 -EVENTO{
 -ID
 -TEXTO
@@ -25,23 +21,25 @@ TODO:
 -ID_EVENTO
 }
 """
-
+#refatorar com oop depois, antes de criar certificados e eventos
 dados: Any=[
     {
         "id_participante":1,
         "CPF":"555.555.555.55",
-        "Nome":"Fulano da Silva",
+        "Nome":"Carlos",
 
     },
     {   "id_participante":2,
-        "CPF":"666.666.666.66",
-        "Nome":"Capetilson da Silva",
+        "CPF":"777.777.777.77",
+        "Nome":"Nicolas",
+    },
+        {   "id_participante":3,
+        "CPF":"123.123.123.45",
+        "Nome":"Ravel",
     }
+
 ]
 
-@app.get("/")
-async def root():
-    return {"message":"Hello World"}
 
 @app.get("/participantes")
 async def read_participantes():
@@ -54,10 +52,10 @@ async def read_participante(id:int):
             return {"participantes":participante}
     raise HTTPException(status_code=404)
 
-@app.post("/participantes/adicionar")
+@app.post("/participantes/adicionar") #depois preciso procurar uma forma mais rapida de testar os metodos, ta um saco adicionar cada campo manualmente
 async def criar_participante(body: dict[str,Any]):
     novo: Any = {
-        "id_participante":randint(100,1000),
+        "id_participante":randint(100,1000), #aleatorio apenas por enq, so pra nao adicionar mais um input de usuario
         "CPF":body.get("CPF"),
         "Nome":body.get("Nome"),
 
@@ -67,7 +65,7 @@ async def criar_participante(body: dict[str,Any]):
 
 @app.put("/participantes/editar/{id}")
 async def editar_participantes(id: int,body: dict[str,Any]):
-    for indice, participante in enumerate(dados):
+    for indice, participante in enumerate(dados): #separa em (index,participante)
         if participante.get("id_participante") == id:
             atualizado: Any = {
                 "id_participante":id,
